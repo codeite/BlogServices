4969bf5a764f86c53839de1a1b33ba7ba622e625
Sam Plews <sam.plews@codeite.com>
2014-04-17 17:50:11 +0100

    Publish

--- Codeite.Blog.Services/App_Start/RouteConfig.cs
+++ Codeite.Blog.Services/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Codeite.Blog.Services
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Converter", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
